2283b6f8fff7095ee49576b74433200a2d233930
Natan <xpenatan@gmail.com>
2024-09-15 21:40:12 -0300

    Fix typo

--- feature/lib/ecs/src/main/java/xlight/engine/ecs/component/XComponent.java
+++ feature/lib/ecs/src/main/java/xlight/engine/ecs/component/XComponent.java
@@ -5,13 +5,13 @@ import xlight.engine.ecs.entity.XEntity;
 
 public interface XComponent {
     /**
-     * Called when a component is attached to component.
+     * Called when a component is attached to an entity.
      * Will be called if entity is attached or detached
      */
     default void onAttach(XWorld world, XEntity entity) {}
 
     /**
-     * Called when a component is detached to component.
+     * Called when a component is detached from an entity.
      * Will be called if entity is attached or detached
      */
     default void onDetach(XWorld world, XEntity entity) {}
