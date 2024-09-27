ff1b08022c6739b1904845f2947b4206692abf07
Natan <xpenatan@gmail.com>
2024-09-27 16:08:28 -0300

    Test density

--- engine/web/src/main/java/xlight/engine/core/XWebApp.java
+++ engine/web/src/main/java/xlight/engine/core/XWebApp.java
@@ -1,11 +1,14 @@
 package xlight.engine.core;
 
 import com.github.xpenatan.gdx.backends.teavm.TeaApplication;
+import com.github.xpenatan.gdx.backends.teavm.TeaGraphics;
 import xlight.engine.impl.XEngineImpl;
 
 public class XWebApp {
 
     public XWebApp(XApplication applicationListener, XWebConfiguration config) {
-        new TeaApplication(new XApplicationInternal(new XEngineImpl(), applicationListener), config);
+        TeaApplication teaApplication = new TeaApplication(new XApplicationInternal(new XEngineImpl(), applicationListener), config);
+        double nativeScreenDensity = ((TeaGraphics)teaApplication.getGraphics()).getNativeScreenDensity();
+        System.out.println("NativeScreenDensity: " + nativeScreenDensity);
     }
 }
